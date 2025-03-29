936568cc88854ab3d4b3630f4b403823fbba2b58
asiye <nouriasiye9@gmail.com>
2025-03-28 17:46:14 -0700

    add Database class

---
+++ src/db/Database.java
@@ -0,0 +1,37 @@
+package db;
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+public class Database {
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    private static int Id = 1;
+
+    public static void add(Entity e){
+        e.id = Id++;
+        entities.add(e);
+
+    }
+    public static Entity get(int id) {
+        for(Entity e: entities){
+            if(e.id == id){
+                return e;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+    public static void delete(int id)  {
+        for(Entity e: entities){
+            if(e.id == id){
+                entities.remove(e);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+    public static void update(Entity e)  {
+        Entity existing = get(e.id);
+        entities.set(entities.indexOf(existing), e);
+    }
+}
+
